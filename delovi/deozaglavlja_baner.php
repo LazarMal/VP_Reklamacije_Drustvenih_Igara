2ae4d60929323c1b7439503ec47764495f25b1c2
LazarMal <lazarmalesev.9@gmail.com>
2026-08-26 13:53:00 +0200

    Folder slike promena imena

--- delovi/deozaglavlja_baner.php
+++ delovi/deozaglavlja_baner.php
@@ -1,6 +1,6 @@
 
 <meta charset="UTF-8">
-<table style="width:100%; height=100% padding:0" align="center" cellspacing="0" cellpadding="0" border="0" background="images/banerTF.png" no-repeat>
+<table style="width:100%; height=100% padding:0" align="center" cellspacing="0" cellpadding="0" border="0" background="slike/banerTF.png" no-repeat>
 <tr>
 <td><font color="white" size="1px">.</font>
 </td>
